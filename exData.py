39132527156c1ba7435b83ae7b9078b88d1226e1
Talha Khalil <tkhalil0703@gmail.com>
2019-06-14 14:44:00 -0600

    Extracting Data for analysis

--- exData.py
+++ exData.py
@@ -1,6 +1,21 @@
 import scipy.io as sio
+import matplotlib.pyplot as plt
 content = sio.loadmat('/Users/talhakhalil/Desktop/Research/Data/2120/2120-1218auto1_fooof_results.mat')
 struct = content['fooof_results']
 val = struct[0,0]
-print (val['background_params'])
-print (val['peak_params'])
+exponential = val['background_params'][0][1]
+error = val['error'][0][0]
+r2 = val['r_squared'][0][0]
+peak = val['peak_params']
+peakvalues = []
+for x in peak:
+    peakvalues.append(x[0])
+
+#plt.scatter(0,error)
+#plt.show()
+print(exponential)
+print(error)
+print(r2)
+print(peakvalues)
+#print (val['background_params'])
+#print (val['peak_params'])
