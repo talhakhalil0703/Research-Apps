290f3c71c13f5180146a681187b07384857afb6c
Talha Khalil <tkhalil0703@gmail.com>
2019-07-15 09:55:30 -0600

    backup before pc reset

--- noteMaker/gui.py
+++ noteMaker/gui.py
@@ -12,6 +12,7 @@ notes_content = ''
 notes_path = 'notes.txt'
 content_to_write = 'zxczxc'
 
+old_time_name = ''
 patient_number = ''
 time_start = ''
 time_end = ''
@@ -52,7 +53,7 @@ mm_start_text_box.grid(column = 3, row = 1)
 mm_end_text_box.grid(column = 5, row = 1)
 type_of_test_text_box.grid(column = 1, row = 1)
 
-console = scrolledtext.ScrolledText(window, height = 10)
+console = scrolledtext.ScrolledText(window, height = 35)
 console.grid(column = 0, columnspan = 6, row = 3)
 
 time_regex = re.compile(r'(\d\d)(:)(\d\d)(:)(\d\d)')
@@ -96,8 +97,14 @@ def insert_values(patient_number, time_start, time_end, mm_start, mm_end, type_o
     type_of_test_text_box.insert(END, type_of_test)
 
 def generate_string():
-    time_name = time_regex.search(time_start_text_box.get())
-    time_name = time_name[1] + time_name[3]
+    time_name = ''
+    if time_start_text_box.get() != '':
+        global old_time_name
+        time_name = time_regex.search(time_start_text_box.get())
+        time_name = time_name[1] + time_name[3]
+        if time_name == old_time_name:
+            time_name += 'B'
+        old_time_name = time_name
     return patient_number_text_box.get() + '-' + time_name + '  ' + time_start_text_box.get() + '  ' + time_end_text_box.get() + '  ' + mm_start_text_box.get() + ' to ' + mm_end_text_box.get() + '  ' + type_of_test_text_box.get() + '\n'
 
 def exit_clicked():
@@ -109,8 +116,12 @@ def write_clicked():
     insert_values(patient_number, time_start, time_end, mm_start, mm_end, type_of_test)
     read_notes_file()
 
-def next_trajectory_clicked():
+def comment_button_clicked():
+    write_to_notes_file(type_of_test_text_box.get())
     write_to_notes_file('\n')
+    insert_values('','','','','','')
+    read_notes_file()
+def next_trajectory_clicked():
     insert_values('','','','','','')
     read_notes_file()
 
@@ -126,7 +137,9 @@ write_button = Button(window, text = 'Write', command = write_clicked)
 remove_last_button = Button(window, text = 'Remove Last', command = remove_last_clicked)
 next_trajectory = Button(window, text = 'Next Trajectory', command = next_trajectory_clicked)
 read_button = Button(window, text = 'Print File', command = read_button_clicked)
-exit_button.grid(column = 0, row = 2, columnspan = 2)
+comment_button = Button(window, text= 'Just Comment', command = comment_button_clicked)
+exit_button.grid(column = 0, row = 2)
+comment_button.grid(column = 1, row = 2)
 write_button.grid(column = 2, row = 2)
 remove_last_button.grid(column = 3, row = 2)
 next_trajectory.grid(column = 4, row = 2)
